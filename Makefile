5ab84bdc96c66ee3da7f7b89d677bb7a7a57ccec
Alain Sene <asene@c3r4p8.42perpignan.fr>
2024-11-04 14:37:14 +0100

    itoa

--- Makefile
+++ Makefile
@@ -28,7 +28,8 @@ SOURCES = ft_isalpha.c \
 		ft_substr.c \
 		ft_strjoin.c \
 		ft_strtrim.c \
-		ft_split.c
+		ft_split.c \
+		ft_itoa.c
 
 OBJS = $(SOURCES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -38,5 +38,5 @@ char	*ft_substr(char const *str, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *str, char const *set);
 char	**ft_split(char const *s, char c);
-
+char	*ft_itoa(int n);
 #endif
