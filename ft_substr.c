74da9d0454159911d7a9d5954f2f265ab8291303
Alain Sene <asene@c3r3p6.42perpignan.fr>
2024-11-05 09:43:42 +0100

    fi substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:16:10 by asene             #+#    #+#             */
-/*   Updated: 2024/11/04 14:46:43 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/05 09:42:49 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 	char	*new_str;
 
 	i = 0;
-	ft_strlen(str);
 	new_str = malloc(sizeof(char) * (len + 1));
 	if (new_str == NULL)
 		return (NULL);
-	while (i < len && str[start + i])
+	if (start < (unsigned int)ft_strlen(str))
 	{
-		new_str[i] = str[start + i];
-		i++;
+		while (i < len && str[start + i])
+		{
+			new_str[i] = str[start + i];
+			i++;
+		}
 	}
 	new_str[i] = '\0';
 	return (new_str);
