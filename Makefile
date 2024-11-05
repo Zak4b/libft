6736ececc7c2a08aa9cf497708d8fb7ec721d524
Alain Sene <zakab.66@gmail.com>
2024-11-05 13:47:41 +0100

    lstmap

--- Makefile
+++ Makefile
@@ -45,7 +45,8 @@ BONUS_SOURCES = ft_lstnew_bonus.c \
 		ft_lstclear_bonus.c \
 		ft_lstdelone_bonus.c \
 		ft_lstclear_bonus.c \
-		ft_lstiter_bonus.c
+		ft_lstiter_bonus.c \
+		ft_lstmap_bonus.c
 
 OBJS = $(SOURCES:.c=.o)
 BONUS_OBJS = $(BONUS_SOURCES:.c=.o)
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:11:59 by asene             #+#    #+#             */
-/*   Updated: 2024/11/04 15:12:00 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/05 13:45:47 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
+	t_list	*new_item;
 
+	if (!lst || !f || !del)
+		return (NULL);
 	new_list = malloc(sizeof(t_list));
 	if (new_list == NULL)
 		return (NULL);
 	new_list->next = NULL;
 	while (lst)
 	{
-		ft_lstadd_back(ft_lstnew(f(lst->content)), new_list);
+		new_item = ft_lstnew(f(lst->content));
+		ft_lstadd_back(&new_list, new_item);
 		lst = lst->next;
 	}
-	del(lst);
 	return (new_list);
 }
