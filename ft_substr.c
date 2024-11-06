a381df13f12e0d421f2df4908bd79b3c9318a27b
Alain Sene <zakab.66@gmail.com>
2024-11-06 15:49:44 +0100

    substr malloc size

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:16:10 by asene             #+#    #+#             */
-/*   Updated: 2024/11/05 09:42:49 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/06 15:27:03 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,22 @@
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	size_t	i;
+	size_t	src_len;
 	char	*new_str;
 
-	i = 0;
+	src_len = ft_strlen(str);
+	if (start >= src_len)
+		len = 0;
+	else if (len > src_len - start)
+		len = src_len - start;
 	new_str = malloc(sizeof(char) * (len + 1));
 	if (new_str == NULL)
 		return (NULL);
-	if (start < (unsigned int)ft_strlen(str))
+	i = 0;
+	while (i < len && str[start + i])
 	{
-		while (i < len && str[start + i])
-		{
-			new_str[i] = str[start + i];
-			i++;
-		}
+		new_str[i] = str[start + i];
+		i++;
 	}
 	new_str[i] = '\0';
 	return (new_str);
