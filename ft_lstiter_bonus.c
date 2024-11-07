e1d94fc3ba8ea97fed4ddd7668d8ef1771093a1a
Alain Sene <zakab.66@gmail.com>
2024-11-07 15:49:02 +0100

    fix lstiter

--- ft_lstiter_bonus.c
+++ ft_lstiter_bonus.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:11:52 by asene             #+#    #+#             */
-/*   Updated: 2024/11/04 15:11:53 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/07 15:47:09 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
+	if (!lst || !f)
+		return ;
 	f(lst->content);
 	if (lst->next)
 		ft_lstiter(lst->next, f);
