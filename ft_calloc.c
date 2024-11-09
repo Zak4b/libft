792608154cfec4b3a4567c95017c257a7fb89009
alainsn66 <zakab.66gmail.com>
2024-11-09 12:14:21 +0100

    facto calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,29 +6,26 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:12:50 by asene             #+#    #+#             */
-/*   Updated: 2024/11/06 14:54:08 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/09 12:07:58 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "stdint.h"
 
 void	*ft_calloc(size_t count, size_t element_size)
 {
 	void	*ptr;
 	size_t	total_size;
 
+	if (SIZE_MAX / element_size < count)
+		return (NULL);
 	if (count == 0 || element_size == 0)
-	{
-		count = 1;
-		element_size = 1;
-	}
-	total_size = count * element_size;
-	if (total_size / count != element_size)
-		return (NULL);
-	ptr = malloc(total_size);
-	if (ptr == NULL)
-		return (NULL);
+		total_size = 1;
 	else
+		total_size = count * element_size;
+	ptr = malloc(total_size);
+	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
 	return (ptr);
 }
