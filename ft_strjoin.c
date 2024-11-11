adce67b6ac5e76b687aad1d9371993886b4d2610
Alain Sene <zakab.66@gmail.com>
2024-11-11 12:19:28 +0100

    facto

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: asene <asene@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 15:15:13 by asene             #+#    #+#             */
-/*   Updated: 2024/11/04 15:15:13 by asene            ###   ########.fr       */
+/*   Updated: 2024/11/11 12:17:55 by asene            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	i = 0;
 	while (*s1)
-	{
-		new_str[i++] = *s1;
-		s1++;
-	}
+		new_str[i++] = *(s1++);
 	while (*s2)
-	{
-		new_str[i++] = *s2;
-		s2++;
-	}
+		new_str[i++] = *(s2++);
 	new_str[i] = '\0';
 	return (new_str);
 }
